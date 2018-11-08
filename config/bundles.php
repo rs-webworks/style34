081a6d6be27e11c8c66117f15ec16df5f28830ee
Martin Jagr <martin.jagr@telia.no>
2018-11-08 07:34:45 +0100

    Updates to frontend template

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];
